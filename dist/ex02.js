7507c16d6fb5c71689e7a964f1aa44eb85d48b59
Rodrigo237 <36214941+Rodrigo237@users.noreply.github.com>
2025-10-01 11:48:30 -0700

    exercise done

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,14 +1,13 @@
+"use strict";
 // 2. Create a function named `isPalindrome` that takes a string as a parameter.
 //    The function should return `true` if the string is a palindrome (reads the same backward as forward),
 //    otherwise, return `false`.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function isPalindrome(word: string): boolean {
-    const textInput: string = word.toLowerCase().replace(/[^a-z0-9]/gi, '');
-    const reverse: string = textInput.split('').reverse().join('');
-    return textInput === reverse
+function isPalindrome(word) {
+    const textInput = word.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    const reverse = textInput.split('').reverse().join('');
+    return textInput === reverse;
 }
-
 // Expected output:
 console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("hello"));   // false
+console.log(isPalindrome("hello")); // false
--- dist/ex05.js
+++ dist/ex05.js
@@ -1,20 +1,18 @@
+"use strict";
 // 5. Create a function named `countVowels` that takes a string as a parameter.
 //    The function should return the number of vowels (a, e, i, o, u) in the string.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function countVowels(word: string): number {
-    const vowels = ['a','e','i','o','u'];
+function countVowels(word) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
     const text = word.toLowerCase();
-    let count = 0
-
-    for(const char of text){
-        if(vowels.indexOf(char) !== -1){
+    let count = 0;
+    for (const char of text) {
+        if (vowels.indexOf(char) !== -1) {
             count++;
         }
     }
     return count;
 }
-
 // Expected output:
-console.log(countVowels("hello"));    // 2
-console.log(countVowels("typescript"));// 2
+console.log(countVowels("hello")); // 2
+console.log(countVowels("typescript")); // 2
